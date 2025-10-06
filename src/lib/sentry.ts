dcd1db1a0735b1a7c539f0e1ad234afd822e862f
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-10-06 15:14:15 -0300

    refactor: remove unnecessary comment in sentry.ts

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import * as Sentry from "@sentry/nestjs";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
-// Ensure to call this before importing any other modules!
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
