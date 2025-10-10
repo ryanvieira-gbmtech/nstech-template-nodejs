659a2d54dcb7c13242c0583860f7281b2c0e5190
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-10-10 15:33:06 -0300

    feat: update FindProductsResponseDTO to reflect product details in API response

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
 export default async () => {
     const t = {};
-    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./infra/prometheus/metric.controller"), { "MetricController": { "index": { type: String } } }], [import("./modules/example/http/controllers/example.controller"), { "ExampleController": { "findTerminalShifts": {} } }], [import("./infra/bucket/bucket.controller"), { "BucketController": { "generateUploadUrl": {} } }]] } };
+    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./infra/prometheus/metric.controller"), { "MetricController": { "index": { type: String } } }], [import("./modules/example/http/controllers/example.controller"), { "ExampleController": { "findProducts": {} } }], [import("./infra/bucket/bucket.controller"), { "BucketController": { "generateUploadUrl": {} } }]] } };
 };
--- src/modules/example/http/controllers/dto/example-response.ts
+++ src/modules/example/http/controllers/dto/example-response.ts
@@ -5,11 +5,15 @@ export class FindProductsResponseDTO {
   @Exclude() // vamos supor que o id não é necessário na resposta
   id!: number;
 
-  @ApiProperty({ example: "01X07", description: "Turno do terminal" })
+  @ApiProperty({ example: "Soja", description: "Nome do produto" })
   @Expose()
   name!: string;
 
-  @ApiProperty({ example: "01:00", description: "Hora de início do turno" })
+  @ApiProperty({
+    format: "uuid",
+    example: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",
+    description: "ID externo do produto",
+  })
   @Expose()
   externalId!: string;
 }
