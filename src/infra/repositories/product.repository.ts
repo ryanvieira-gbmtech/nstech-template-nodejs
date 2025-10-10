921a5ec272baffc5325dc940f8554fc9624c8147
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-10-10 15:20:38 -0300

    feat: implement findAll method in ProductRepository for product retrieval with optional name filter

--- src/infra/repositories/product.repository.ts
+++ src/infra/repositories/product.repository.ts
@@ -1,12 +1,23 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { isNotEmpty } from "class-validator";
 import { Kysely } from "kysely";
 import { DATABASE_CONNECTION } from "../database/database.module";
 import Database from "../database/schema/Database";
-import { NewProduct } from "../database/schema/public/Product";
+import { NewProduct, Product } from "../database/schema/public/Product";
 
 @Injectable()
 export class ProductRepository {
   constructor(@Inject(DATABASE_CONNECTION) private db: Kysely<Database>) {}
+
+  async findAll(filter: Partial<Pick<Product, "name">>) {
+    const products = await this.db
+      .selectFrom("product")
+      .$if(isNotEmpty(filter.name), (qb) => qb.where("name", "ilike", `%${filter.name}%`))
+      .selectAll()
+      .execute();
+
+    return products;
+  }
 
   async create(data: NewProduct) {
     return this.db.insertInto("product").values(data).returningAll().executeTakeFirstOrThrow();
