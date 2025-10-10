35ee2de1c9dc9a996b7ce1622757cf62a393f14f
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-10-10 14:56:06 -0300

    refactor: simplify findTerminalShifts method by removing unused parameters and imports

--- src/modules/example/http/controllers/example.controller.ts
+++ src/modules/example/http/controllers/example.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, Headers, Param, Query, SerializeOptions } from "@nestjs/common";
+import { Controller, Get, SerializeOptions } from "@nestjs/common";
 import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
-import { TerminalsId } from "@/infra/database/schema/public/Terminals";
-import { HeadersDTO } from "@/shared/dto";
 import { ExampleService } from "../../services/example.service";
-import { FindTerminalShiftsRequestDTO } from "./dto/example-request";
 import { FindTerminalShiftsResponseDTO } from "./dto/example-response";
 
 @ApiTags("example")
 @Controller("example")
 export class ExampleController {
-  constructor(private readonly exampleService: ExampleService) {}
+  constructor(readonly _exampleService: ExampleService) {}
 
   @Get(":terminalId/shifts")
   @ApiOperation({ summary: "Busca turnos do terminal" })
@@ -19,13 +16,5 @@ export class ExampleController {
     description: "Lista de turnos do terminal",
   })
   @SerializeOptions({ type: FindTerminalShiftsResponseDTO })
-  async findTerminalShifts(
-    @Param("terminalId") terminalId: string,
-    @Query() filter: FindTerminalShiftsRequestDTO,
-    @Headers() headers: HeadersDTO,
-  ) {
-    const id = Number(terminalId) as TerminalsId;
-
-    return this.exampleService.findTerminalShifts(id, filter, headers);
-  }
+  async findTerminalShifts() {}
 }
