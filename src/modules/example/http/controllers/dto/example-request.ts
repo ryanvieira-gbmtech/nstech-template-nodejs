eee3e92ced25326dd7fe9a91b3e31e1bac8ab9da
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-10-10 15:14:25 -0300

    feat: rename FindTerminalShiftsRequestDTO to FindProductsRequestDTO and update properties for product search

--- src/modules/example/http/controllers/dto/example-request.ts
+++ src/modules/example/http/controllers/dto/example-request.ts
@@ -1,8 +1,12 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class FindTerminalShiftsRequestDTO {
-  @ApiPropertyOptional({ example: "01X07", description: "Turno do terminal", default: "01X07" })
+export class FindProductsRequestDTO {
+  @ApiPropertyOptional({
+    example: "Produto A",
+    description: "Nome do produto",
+    default: "Produto A",
+  })
   @IsString()
   @IsNotEmpty()
   @IsOptional()
