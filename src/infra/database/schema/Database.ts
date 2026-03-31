0ff1ec78b24ddda829803d262d612792f3f5c51b
Ryan Vieira <ryanvsouza.dev@gmail.com>
2026-03-31 09:53:06 -0300

    chore: remove product table migration and related schema files

--- src/infra/database/schema/Database.ts
+++
@@ -1,5 +0,0 @@
-import type { default as PublicSchema } from "./public/PublicSchema";
-
-type Database = PublicSchema;
-
-export default Database;
--- src/infra/database/schema/public/PublicSchema.ts
+++
@@ -1,5 +0,0 @@
-import type { default as ProductTable } from "./Product";
-
-export default interface PublicSchema {
-  product: ProductTable;
-}
